a4b20705a7c690f78c38c4a36bd18a004998605e
Zied Yousfi <zied.essaber@gmail.com>
2026-02-12 15:53:40 +0100

    feat(config): update plugin and settings files across nvim, yazi, and windows

--- dotfiles/nvim/.config/nvim/lua/plugins/language_features.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/language_features.lua
@@ -1,75 +1,78 @@
 return {
-  -- Mason: package manager for LSP servers, linters, formatters
-  {
-    "mason-org/mason.nvim",
-    lazy = false,
-    opts = {},
-  },
+	-- Mason: package manager for LSP servers, linters, formatters
+	{
+		"mason-org/mason.nvim",
+		lazy = false,
+		opts = {},
+	},
 
-  -- Mason-LSPConfig: auto-install and enable LSP servers
-  {
-    "mason-org/mason-lspconfig.nvim",
-    lazy = false,
-    dependencies = {
-      "mason-org/mason.nvim",
-      "neovim/nvim-lspconfig",
-    },
-    opts = {
-      automatic_installation = true,
-      automatic_enable = true,
-    },
-  },
+	-- Mason-LSPConfig: auto-install and enable LSP servers
+	{
+		"mason-org/mason-lspconfig.nvim",
+		lazy = false,
+		dependencies = {
+			"mason-org/mason.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		opts = {
+			automatic_installation = true,
+			automatic_enable = true,
+		},
+	},
 
-  -- LSPConfig: configurations for LSP servers
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    dependencies = {
-      "mason-org/mason-lspconfig.nvim",
-      "Saghen/blink.cmp",
-    },
-    config = function()
-      local capabilities = require("blink.cmp").get_lsp_capabilities()
-      local lspconfig = require("lspconfig")
-      lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
-        capabilities = capabilities,
-      })
-    end,
-  },
+	-- LSPConfig: configurations for LSP servers
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		dependencies = {
+			"mason-org/mason-lspconfig.nvim",
+			"Saghen/blink.cmp",
+		},
+		config = function()
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
+			local lspconfig = require("lspconfig")
+			lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
+				capabilities = capabilities,
+			})
+		end,
+	},
 
-  -- Autocompletion
-  {
-    "Saghen/blink.cmp",
-    lazy = false,
-    dependencies = "rafamadriz/friendly-snippets",
-    version = "*",
-    opts = {
-      keymap = { preset = "default" },
-      appearance = {
-        use_nvim_cmp_as_default = true,
-        nerd_font_variant = "mono",
-      },
-      sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
-      },
-    },
-  },
+	-- Autocompletion
+	{
+		"Saghen/blink.cmp",
+		lazy = false,
+		dependencies = "rafamadriz/friendly-snippets",
+		version = "*",
+		opts = {
+			keymap = { preset = "enter" },
+			appearance = {
+				use_nvim_cmp_as_default = true,
+				nerd_font_variant = "mono",
+			},
+			completion = {
+				documentation = { auto_show = true },
+			},
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+		},
+	},
 
-
-  -- Treesitter: syntax highlighting and code understanding
-  {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    build = ":TSUpdate",
-  },
-  {
-    'stevearc/conform.nvim',
-    opts = {
-      format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 500,
-        lsp_format = "fallback",
-      },
-    },
-  }
+	-- Treesitter: syntax highlighting and code understanding
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		build = ":TSUpdate",
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+		},
+	},
 }
