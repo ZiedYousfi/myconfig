ba6783651a0796ba765dde8b4bbd1f2bed9937c2
Zied Yousfi <zied.essaber@gmail.com>
2026-02-12 18:27:03 +0100

    feat(buffers): add flash.nvim with keymap & refactor plugin table

--- dotfiles/nvim/.config/nvim/lua/plugins/buffers.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/buffers.lua
@@ -1,10 +1,26 @@
 return {
-{
-    "rachartier/tiny-inline-diagnostic.nvim",
-    event = "VeryLazy",
-    priority = 1000,
-    config = function()
-        require("tiny-inline-diagnostic").setup()
-        vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
-    end,
-}}
+	{
+		"rachartier/tiny-inline-diagnostic.nvim",
+		event = "VeryLazy",
+		priority = 1000,
+		config = function()
+			require("tiny-inline-diagnostic").setup()
+			vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
+		end,
+	},
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{
+				"f",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+		},
+	},
+}
